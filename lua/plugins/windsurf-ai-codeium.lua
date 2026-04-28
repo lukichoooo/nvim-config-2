ae03cf4d4667d38e8f6aea1b10b5549cbaa523b1
Luka <khundadzeluka792@gmail.com>
2026-04-28 19:23:25 +0400

    codeium ai suggestion color improvement

--- lua/plugins/windsurf-ai-codeium.lua
+++ lua/plugins/windsurf-ai-codeium.lua
@@ -5,5 +5,6 @@ return {
     vim.g.codeium_disable_bindings = 1
     vim.g.codeium_manual = false -- change to false for manual autocomplete
     vim.g.codeium_idle_delay = 0
+    vim.api.nvim_set_hl(0, "CodeiumSuggestion", { fg = "#aaaaaa", bg = "#1e1e2e" })
   end,
 }
