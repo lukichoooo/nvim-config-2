807c544d5993b9e7324b55c8ee12cfa0bc4e1a08
Luka <khundadzeluka792@gmail.com>
2026-06-02 18:48:03 +0400

    ...

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "clangd",
+      "codelldb",
+      "intelephense",
+      "lua-language-server",
+      "phpactor",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
